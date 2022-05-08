9716fd37e9c1fc7b19595fe43f483fd66e651398
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-09 04:26:18 +0530

    add - animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,28 @@ module.exports = {
     theme: {
         extend: {
             keyframes: {
+                "move-in-top": {
+                    "0%": { transform: "translateY(-25%)", opacity: "0" },
+                    "80%": { transform: "translateY(5%)" },
+                    "100%": { transform: "translateY(0)", opacity: "1" },
+                },
+                "move-in-right": {
+                    "0%": { transform: "translateX(-25%)", opacity: "0" },
+                    "80%": { transform: "translateX(5%)" },
+                    "100%": { transform: "translateX(0)", opacity: "1" },
+                },
                 "fade-in": {
                     "0%": { opacity: "0" },
-                    "100%": { opacity: "1" },
+                    100: { opacity: "1" },
                 },
+            },
+            animation: {
+                "move-in-top": "move-in-top 1s ease-in-out backwards",
+                "move-in-right": "move-in-right 1s ease-in-out backwards",
+                "fade-in": "fade-in 0.5s ease-in-out backwards",
             },
             rotate: {
                 360: "360deg",
-            },
-            animation: {
-                "fade-in": "fade-in 0.5s ease-in-out backwards",
             },
         },
     },
