ff67afd605fc8bbab8dd16ab819fe2754b994610
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-17 15:41:03 +0530

    add - toast container

--- src/routes/ConditionalRouter.jsx
+++ src/routes/ConditionalRouter.jsx
@@ -5,6 +5,9 @@ import { QuestionsPage } from "routes/QuestionsPage";
 import { ProfilePage } from "routes/ProfilePage";
 import { PullRequestsPage } from "./PullRequestsPage";
 import { HomePage } from "./HomePage";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import { SingleQuestionsPage } from "./SingleQuestionPage";
 import { MyPullRequests } from "components/Profile/MyPullRequests";
@@ -31,36 +34,62 @@ export const ConditionalRouter = () => {
         return listenerCleaner;
     }, [dispatch]);
     return (
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route index element={<Navigate to={"/home"} replace />} />
-                <Route path="questions" element={<QuestionsPage />} />
-                <Route path="pull-requests" element={<PullRequestsPage />} />
-                <Route
-                    path="question/:questionId"
-                    element={<SingleQuestionsPage />}
-                />
+        <>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<Navigate to={"/home"} replace />} />
+                    <Route path="questions" element={<QuestionsPage />} />
+                    <Route
+                        path="pull-requests"
+                        element={<PullRequestsPage />}
+                    />
+                    <Route
+                        path="question/:questionId"
+                        element={<SingleQuestionsPage />}
+                    />
 
-                <Route element={<ProtectedRoute currentUser={currentUser} />}>
-                    <Route path="profile" element={<ProfilePage />}>
-                        <Route
-                            index
-                            element={<Navigate to={"my-pr-links"} replace />}
-                        />
-                        <Route
-                            path="my-pr-links"
-                            element={<MyPullRequests />}
-                        />
-                        <Route path="my-questions" element={<MyQuestions />} />
-                        <Route path="my-answers" element={<MyAnswers />} />
-                        <Route path="my-comments" element={<MyComments />} />
+                    <Route
+                        element={<ProtectedRoute currentUser={currentUser} />}>
+                        <Route path="profile" element={<ProfilePage />}>
+                            <Route
+                                index
+                                element={
+                                    <Navigate to={"my-pr-links"} replace />
+                                }
+                            />
+                            <Route
+                                path="my-pr-links"
+                                element={<MyPullRequests />}
+                            />
+                            <Route
+                                path="my-questions"
+                                element={<MyQuestions />}
+                            />
+                            <Route path="my-answers" element={<MyAnswers />} />
+                            <Route
+                                path="my-comments"
+                                element={<MyComments />}
+                            />
+                        </Route>
                     </Route>
                 </Route>
-            </Route>
 
-            <Route element={<ProtectedAuth currentUser={currentUser} />}>
-                <Route path="/home" element={<HomePage />} />
-            </Route>
-        </Routes>
+                <Route element={<ProtectedAuth currentUser={currentUser} />}>
+                    <Route path="/home" element={<HomePage />} />
+                </Route>
+            </Routes>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                theme="light"
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </>
     );
 };
