d05bff930ca2a61f1add579b08df4a4d2b5f777d
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-17 21:58:56 +0530

    add - bookmarkIcon

--- src/assets/Icons/Icons.jsx
+++ src/assets/Icons/Icons.jsx
@@ -38,6 +38,23 @@ export const CheckIcon = ({ className }) => {
         </svg>
     );
 };
+export const BookmarkIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}>
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+            />
+        </svg>
+    );
+};
 export const CaretUpFillIcon = ({ className }) => {
     return (
         <svg
