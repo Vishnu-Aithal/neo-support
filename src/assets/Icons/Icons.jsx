254e92cd6f707a0489499495400b62ca14b5cbf6
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-20 13:58:33 +0530

    add - icons

--- src/assets/Icons/Icons.jsx
+++ src/assets/Icons/Icons.jsx
@@ -77,6 +77,35 @@ export const CheckIcon = ({ className }) => {
         </svg>
     );
 };
+export const ExploreIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            fill="currentColor"
+            viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z" />
+        </svg>
+    );
+};
+export const BellIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}>
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+        </svg>
+    );
+};
 export const BookmarkIcon = ({ className }) => {
     return (
         <svg
