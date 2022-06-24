3f4098674225aab18615d3c8e4193a32ceaa4890
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-06-24 22:05:11 +0530

    add alt

--- src/components/Previews/UserImage.tsx
+++ src/components/Previews/UserImage.tsx
@@ -1,9 +1,12 @@
-export const UserImage: React.FC<{ src: string }> = ({ src }) => {
+export const UserImage: React.FC<{ src: string; alt: string }> = ({
+    src,
+    alt,
+}) => {
     return (
         <div className="flex items-center justify-start sm:p-2 flex-shrink-0 ">
             <img
                 src={src}
-                alt="random"
+                alt={alt}
                 className=" aspect-square w-16 rounded-full object-cover object-center mb-auto"
             />
         </div>
