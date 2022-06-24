5f5d1b7c9bde201b2197bae6228114b593b3678e
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-06-25 00:57:16 +0530

    fix - empty comments state

--- src/components/Profile/MyComments.tsx
+++ src/components/Profile/MyComments.tsx
@@ -8,18 +8,18 @@ export const MyComments: React.FC = () => {
     const comments = useAppSelector((state) => state.profile.myComments);
 
     const currentUser = useAppSelector((state) => state.currentUser)!;
-
+    console.log(comments);
     useMyComments(currentUser.uid, setMyComments);
     return (
-        <div className="flex flex-col w-11/12 mx-auto gap-2">
-            {comments.length === 0 && (
-                <p className="w-full text-center text-lg font-semibold text-zinc-500">
-                    You have not Commented on any Posts Yet!
-                </p>
-            )}
-            {comments.map((comment) => (
-                <CommentLinks key={comment.uid} commentData={comment} />
-            ))}
-        </div>
+        <>
+            <div className="flex flex-col w-11/12 mx-auto gap-2 peer">
+                {comments.map((comment) => (
+                    <CommentLinks key={comment.uid} commentData={comment} />
+                ))}
+            </div>
+            <p className="w-full text-center text-lg font-semibold text-zinc-500 h-0 overflow-hidden peer-empty:h-auto">
+                You have no comments
+            </p>
+        </>
     );
 };
